eeaeae6c00bcf3d6dd441efd193d97007f80a535
gemy21ce@gmail.com <gemy21ce@gmail.com@b031ea96-d6d3-5f18-613c-af565bf91a20>
2011-01-17 16:41:51 +0000

    bases.

---
+++ controllers/popup.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * 1- check for login status, if logged in, check in session for online friends. if not logged in, shows log in icon with url to webserver application that sends to authinticate and login to facebook, in background page a running script asks for authintication.
+ * 2- shows a list of online friends and shows the last chats.
+ */
+
