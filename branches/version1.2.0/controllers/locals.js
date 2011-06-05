7cefb61542b956928eef8b8e49d66d1190eca502
gemy21ce@gmail.com <gemy21ce@gmail.com@b031ea96-d6d3-5f18-613c-af565bf91a20>
2011-06-05 12:43:25 +0000

    new version using graph api

---
+++ branches/version1.2.0/controllers/locals.js
@@ -0,0 +1,67 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var fbchatlocals={
+    ar:{
+        "onlineFriends":"الأصدقاء المتصلين",
+        "friendList":"قائمة الأصدقاء",
+        "offlineUserChat":" غير متصل الآن، رسائلك ستصل إليه في صندوق البريد الخاص به، إذا كنتم تستخدمون نظام الرسائل الجديد.",
+        "clearField":"إمسح",
+        "logout":"سجل الخروج",
+        "about":"عن الاضافة",
+        "searchfriens":"إبحث في الأصدقاء",
+        "friendsonchat":"الأصدقاء المتصلين",
+        "disconnect":"قطع الإتصال",
+        "send":"إرسل",
+        "settings":"الإعدادت",
+        "extlang":"لغة الإضافة",
+        "playsound":"التنبيه الصوتي",
+        "shownotifications":"تنبيه سطح المكتب",
+        "save":"حفظ",
+        "eng":"الإنجليزية",
+        "ara":"العربية",
+        "savesetting":" تم الحفظ",
+        "DefautlTextAreaValue":"قم بكتابة رسالتك هنا",
+        "chat":"المحادثات",
+        "notifications":"التنبيهات",
+        "gensettings":"إعدادات عامة",
+        "urdisconnected":" أنت الآن غير متصل ",
+        "connect2":" إتصل ؟",
+        "connecting":"جاري الاتصال",
+        "loading":"جاري التحميل",
+        "savebutton":"حفظ",
+        "autoConnect":"اتصال تلقائي"
+    },
+    en:{
+        "onlineFriends":"Online Friends",
+        "friendList":"Friends List",
+        "offlineUserChat":" is offline, your messages will be sent to their inbox, if you both are using the new messages system.",
+        "clearField":"clear",
+        "logout":"Logout",
+        "about":"About",
+        "searchfriens":"Search Friends",
+        "friendsonchat":"Friends on chat",
+        "disconnect":"Go Offline",
+        "send":"Send",
+        "settings":"Settings",
+        "extlang":"Extension Language",
+        "playsound":"Play notification sound",
+        "shownotifications":"Show desktop notification",
+        "save":"Save",
+        "eng":"English",
+        "ara":"Arabic",
+        "savesetting":"Settings Saved",
+        "DefautlTextAreaValue":"type your message here",
+        "chat":"Chat",
+        "notifications":"Notifications",
+        "gensettings":"General",
+        "urdisconnected":"you are offline, ",
+        "connect2":"go online now?",
+        "connecting":"connecting",
+        "loading":"loading",
+        "savebutton":"Save",
+        "autoConnect":"Auto Connect"
+    }
+}
+
